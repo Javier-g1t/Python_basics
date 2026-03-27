646450337a0524935b074911da56a592407714fc
Javier Ramírez <jv.coree@gmail.com>
2026-03-27 16:03:27 -0300

    Functions complete

---
+++ Functions/extra_data_functions.py
@@ -0,0 +1,14 @@
+
+#creando una funcion de 3 parametros 
+#def frase(nombre,apellido,adjetivo):
+#    return f"Hola {nombre} {apellido} , sos muy {adjetivo}"
+
+#utilizando keywords arguments
+#frase_resultante = frase(adjetivo = "capo", nombre = "Lucas", apellido = "Dalto")
+
+#creando la misma funcion con un parametro opcional y un valor por defecto
+def frase(nombre, apellido, adjetivo = "tonto"):
+    return f"Hola {nombre} {apellido} , sos muy {adjetivo}"
+
+frase_resultante = frase("Lucas", "Dalto", "inteligente")
+print(frase_resultante)
